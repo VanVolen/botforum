ed3db0d52f7fdc6d19a70c6efaf7472c7510eae7
VanVolen <brano.sico@gmail.com>
2023-04-26 00:22:41 +0200

    dd

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const axios = require('axios');
-
-console.log("test index js");
-
-
 
 var LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
@@ -24,16 +19,3 @@ app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-
-axios.post('https://ninjaworldrpg.forumotion.me/login', {
-    username: 'Game Master',
-    password: 'admin123',
-    autologin: 'on',
-    login: 'Log in'
-})
-.then((response) => {
-    console.log('Bot se uspesno ulogovao xD');
-})
-.catch((error) => {
-    console.error(error);
-});
