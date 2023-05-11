b97d5c4c020b07f3362d55dbff2103a098360806
nikolapetrovic1 <s.nino.petrovic@gmail.com>
2023-05-11 12:40:38 +0200

    cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors())
+app.use(cors({origin:'https://www.ninjaworldrpg.com'}))
 // app.options('*', cors()) // include before other routes
 
 app.use('/', indexRouter);
