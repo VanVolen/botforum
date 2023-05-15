2c91723315754bb8f4966f72a708ad00a57778b0
VanVolen <brano.sico@gmail.com>
2023-05-15 22:34:42 +0200

    dddd

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/users', usersRouter);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin');
   res.setHeader('Access-Control-Allow-Credentials', true);
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
